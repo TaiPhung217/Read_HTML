e728c9b6a73e9f4d6e618bcb089036231e645842
TaiPhung217 <102504154+TaiPhung217@users.noreply.github.com>
2022-04-13 21:50:47 +0700

    Delete BeautifulSoup.py

--- BeautifulSoup.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open('Html.html','r') as f:
-        contents = f.read()
-        soup = BeautifulSoup(contents,'lxml')
-        for tag in soup:
-                print(f'{tag.text}')
